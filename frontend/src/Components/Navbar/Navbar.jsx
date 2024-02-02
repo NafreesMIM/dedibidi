49d61811503246d4f19d1717388b076d0ff9ead5
NafreesMIM <143109431+NafreesMIM@users.noreply.github.com>
2024-02-02 21:29:57 +0530

    .comEdit update

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -16,17 +16,10 @@ const Navbar = () => {
         <p>DADIBIDI<span className='comEdit'>.COM</span></p>
       </div>
       <ul className="nav-menu">
-      <Dropdown title="SERVICES" items={[' Service 1 ', 'Service 2', 'Service 3','service 4','service 5']} />
+        <Dropdown title="SERVICES" items={[' Service 1 ', 'Service 2', 'Service 3','service 4','service 5']} />
 
-        <li > 
-          <select>
-            <option >SERVICES</option>
-            <option><a href='#'>Option 1</a></option>
-            <option><a href='#'>Option 2</a></option>  
-          </select>
-                
-        </li>
-           42ced7e17d72107e110d4489dec78f60a1049308
+        
+           
         <li >ABOUT US</li>
         <li >CONTACT US</li>
         <li >PRIVACE POLICY</li>
