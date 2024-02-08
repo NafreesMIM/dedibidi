c7f87d54b8cbf2f289362cec2c86df3b3be63172
Adithya Ruwanpura <127475434+Adithyaruwanpura@users.noreply.github.com>
2024-02-08 17:10:23 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from './Components/Home/home';
 import About from './Components/pages/About'; 
 import Contact from './Components/pages/Contact'; 
 import Privacy from './Components/pages/privacy'; 
+import Login from './Components/pages/login'; 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route path='/about' element={<About />} /> 
           <Route path='/contact' element={<Contact />} /> 
           <Route path='/privacy' element={<Privacy />} /> 
+          <Route path='/login' element={<Login />} /> 
         </Routes>
     
         <hr />
