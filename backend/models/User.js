1a1c9d14e5e4e016be3f635acec7e5b958959f58
Adithya Ruwanpura <127475434+Adithyaruwanpura@users.noreply.github.com>
2024-02-19 18:34:00 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,17 +4,11 @@ const userSchema = new mongoose.Schema({
   name: String,
   fullName: String,
   address: String,
-  mobileNumber: String,
-  nicNumber: String,
+  mobileNumber: { type: String, unique: true, required: true },
+  nicNumber: { type: String, unique: true, required: true },
   dateOfBirth: Date,
-  email: String,
+  email: { type: String, unique: true, required: true },
   password: String,
 });
-
-// Static method to check if any of the provided fields already exist
-userSchema.statics.exists = async function (fields) {
-  const user = await this.findOne({ $or: fields }).exec();
-  return !!user;
-};
 
 module.exports = mongoose.model('User', userSchema);
