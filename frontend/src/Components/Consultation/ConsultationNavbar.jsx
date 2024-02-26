b1ab63a71aab8d0f7b2dbe5c273deece66d0cdf0
Adithya Ruwanpura <127475434+Adithyaruwanpura@users.noreply.github.com>
2024-02-26 16:48:27 +0530

    Update ConsultationNavbar.jsx

--- frontend/src/Components/Consultation/ConsultationNavbar.jsx
+++ frontend/src/Components/Consultation/ConsultationNavbar.jsx
@@ -1,12 +1,20 @@
 // ConsultationNavbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../Consultation/ConsultationNavbar.css';
 
 const ConsultationNavbar = () => {
   return (
     <div className="consultation-navbar">
       <Link to="/consultation/home">Home</Link>
       <Link to="/consultation/services">Services</Link>
+      <Link to="/consultation/home">About Us</Link>
+      <Link to="/consultation/services">FAQs</Link>
+      <Link to="/consultation/services">Review</Link>
+      <Link to="/consultation/services">Contact Us</Link>
+      <Link to="/consultation/services">Calender</Link>
+      <Link to="/consultation/services">Payment</Link>
+      <Link to="/consultation/services">Support</Link>
       {/* Add other links specific to the Online Consultation page */}
     </div>
   );
