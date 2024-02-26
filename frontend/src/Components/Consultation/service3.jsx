45d6f66be1fadff43cbd998092c7f2e3f44a6501
Adithya Ruwanpura <127475434+Adithyaruwanpura@users.noreply.github.com>
2024-02-26 14:49:49 +0530

    Create service3.jsx

---
+++ frontend/src/Components/Consultation/service3.jsx
@@ -0,0 +1,17 @@
+// Service3.js
+import React from 'react';
+import ConsultationNavbar from './ConsultationNavbar'; // Adjust the import path based on your project structure
+
+const Service3 = () => {
+    
+  return (
+    <div>
+      <ConsultationNavbar />
+      {/* Your Online Consultation page content */}
+      <h1>Online Consultation Page</h1>
+      {/* Add other content as needed */}
+    </div>
+  );
+};
+
+export default Service3;
