5659b96fe957a9e0d948418c56ae2b6af02dcb94
Adithya Ruwanpura <127475434+Adithyaruwanpura@users.noreply.github.com>
2024-02-26 10:12:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors()); // Allow all origins for demonstration purposes. You should configure it for specific origins.
 
-mongoose.connect('mongodb://localhost:27017/dedibidi', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://127.0.0.1:27017/dedibidi', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
