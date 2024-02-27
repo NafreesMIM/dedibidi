df78ec6b32a6ac7a64811384eaf4168382c26546
Adithya Ruwanpura <127475434+Adithyaruwanpura@users.noreply.github.com>
2024-02-27 12:42:20 +0530

    Update ConsultationNavbar.jsx

--- frontend/src/Components/Consultation/ConsultationNavbar.jsx
+++ frontend/src/Components/Consultation/ConsultationNavbar.jsx
@@ -1,12 +1,31 @@
-// ConsultationNavbar.js
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../Consultation/ConsultationNavbar.css';
+import logo from '../Assets/Logo.png';
 
 const ConsultationNavbar = () => {
   return (
     <div className="consultation-navbar">
-      <Link to="/consultation/home">Home</Link>
+      <div className="navbar-left">
+        <Link to="/consultation/home">
+          <img src={logo} alt="Company Logo" className="logo" />
+        </Link>
+        <h2>Dadibidi Consultation</h2>
+      </div>
+
+      <div className="navbar-center">
+        {/* Add a search bar here */}
+        <input type="text" placeholder="Search..." />
+      </div>
+
+      <div className="navbar-right">
+        {/* Add login and signup links */}
+        <Link to="/consultation/login">Login</Link>
+        <Link to="/consultation/signup">Signup</Link>
+      </div>
+
+      {/* Add other links specific to the Online Consultation page */}
       <Link to="/consultation/services">Services</Link>
       <Link to="/consultation/home">About Us</Link>
       <Link to="/consultation/services">FAQs</Link>
@@ -15,9 +34,9 @@ const ConsultationNavbar = () => {
       <Link to="/consultation/services">Calender</Link>
       <Link to="/consultation/services">Payment</Link>
       <Link to="/consultation/services">Support</Link>
-      {/* Add other links specific to the Online Consultation page */}
     </div>
   );
 };
 
 export default ConsultationNavbar;
+
