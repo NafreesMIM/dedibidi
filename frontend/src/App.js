0897ea2d8878a7cc2719639cb06c32af0523a5fb
NafreesMIM <143109431+NafreesMIM@users.noreply.github.com>
2024-03-07 11:11:38 +0530

    My updates
    Resolved routing Problem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Navbar/Footer';
@@ -8,43 +7,104 @@ import Contact from './Components/Pages/Contact';
 import Privacy from './Components/Pages/privacy'; 
 import Login from './Components/Pages/login'; 
 import Register from './Components/Pages/Register'; 
-import Service3 from './Components/Consultation/service3';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Service3 from './Components/Consultation/service3'; 
+import ConsNavbar from './Components/Consultation/ConsultationNavbar';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <div className='content-container'>
-         <Routes>
-          <Route path='/' element={<Navbar />}>
-            {/* Only render Navbar for routes other than /Consultation/service3 */}
-            <Route index element={<Navbar />} />
-            <Route path='/about' element={<About />} /> 
-            <Route path='/contact' element={<Contact />} /> 
-            <Route path='/privacy' element={<Privacy />} /> 
-            <Route path='/login' element={<Login />} /> 
-            <Route path='/register' element={<Register />} />
-          </Route>
-        </Routes>
-      </div>
-       
-
+        <div>
+          {/* Render Navbar for specific routes */}
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} /> 
-            <Route path='/contact' element={<Contact />} /> 
-            <Route path='/privacy' element={<Privacy />} /> 
-            <Route path='/login' element={<Login />} /> 
-            <Route path='/register' element={<Register />} />
-            <Route path='/Consultation/service3' element={<Service3 />} />
+            <Route path='/' element={<CustomNavbar />} />
+            <Route path='/about' element={<CustomNavbar />} /> 
+            <Route path='/contact' element={<CustomNavbar />} /> 
+            <Route path='/privacy' element={<CustomNavbar />} /> 
+            <Route path='/login' element={<CustomNavbar />} /> 
+            <Route path='/Consultation/*' element={<CustomNavbar />} /> 
+            <Route path='/register' element={<CustomNavbar />} />
           </Routes>
 
-        <hr />
-        <Footer />
+          {/* Render content */}
+          <Routes>
+            <Route path='/' element={<HomeWithContentContainer />} />
+            <Route path='/about' element={<AboutWithContentContainer />} /> 
+            <Route path='/contact' element={<ContactWithContentContainer />} /> 
+            <Route path='/privacy' element={<PrivacyWithContentContainer />} /> 
+            <Route path='/login' element={<LoginWithContentContainer />} /> 
+            <Route path='/register' element={<RegisterWithContentContainer />} />
+            <Route path='/Consultation/*' element={<ConsNavbar />} /> {/* Render ConsNavbar for all consultation pages */}
+            <Route path='/Consultation/service3' element={<Service3 />} /> 
+          </Routes>
+
+          <hr />
+          <Footer />
+        </div>
       </BrowserRouter>
     </div>
   );
+}
+
+// Components with content container for each route
+function HomeWithContentContainer() {
+  return (
+    <div className='content-container'>
+      <Home />
+    </div>
+  );
+}
+
+function AboutWithContentContainer() {
+  return (
+    <div className='content-container'>
+      <About />
+    </div>
+  );
+}
+
+function ContactWithContentContainer() {
+  return (
+    <div className='content-container'>
+      <Contact />
+    </div>
+  );
+}
+
+function PrivacyWithContentContainer() {
+  return (
+    <div className='content-container'>
+      <Privacy />
+    </div>
+  );
+}
+
+function LoginWithContentContainer() {
+  return (
+    <div className='content-container'>
+      <Login />
+    </div>
+  );
+}
+
+function RegisterWithContentContainer() {
+  return (
+    <div className='content-container'>
+      <Register />
+    </div>
+  );
+}
+
+function CustomNavbar() {
+  const location = useLocation();
+  
+  // Check if the current location is under consultation route
+  if (location.pathname.startsWith('/Consultation')) {
+    return null;
+  } else {
+    return <Navbar />;
+  }
 }
 
 export default App;
