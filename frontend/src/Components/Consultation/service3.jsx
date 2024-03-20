12fae699071b2a5b72b66184a01882484ec474a2
Adithya Ruwanpura <127475434+Adithyaruwanpura@users.noreply.github.com>
2024-03-20 12:10:54 +0530

    Update service3.jsx

--- frontend/src/Components/Consultation/service3.jsx
+++ frontend/src/Components/Consultation/service3.jsx
@@ -1,25 +1,32 @@
+// Service3.js
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ConsultationNavbar from './ConsultationNavbar'; // Adjust the import path based on your project structure
-import '../Consultation/service3.css'; // Import your CSS file for styling
+import Home from './Home'; // Import the component for the Home page
+import AboutUs from './about'; // Import the component for the About Us page
+
 
 const Service3 = () => {
   return (
     <div className="service3-container">
-      <ConsultationNavbar />
+      <BrowserRouter>
+        <ConsultationNavbar />
 
-      {/* Main Content */}
-      <div className="main-content">
-        {/* Your Online Consultation page content */}
-        <h2>Welcome to the Dedibidi <br></br> 
-         Consultation Service!!!</h2>
-        {/* Add other content as needed */}
-       
-        <button>Explore More</button>
-      </div>
-
-    
+        {/* Main Content */}
+        <div className="main-content">
+          <Routes>
+            <Route path="/consultation/home" element={<Home />} />
+            <Route path="/consultation/about" element={<AboutUs />} />
+            {/* Add routes for other pages */}
+          </Routes>
+          <h2>Welcome to the Dedibidi Consultation Service!!!</h2>
+          {/* Add other content as needed */}
+          <button>Explore More</button>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
 
 export default Service3;
+
