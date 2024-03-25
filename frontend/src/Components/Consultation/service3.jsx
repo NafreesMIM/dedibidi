b698e802ebe62a73bb3cd9fc5868cd4db9acf909
Adithya Ruwanpura <127475434+Adithyaruwanpura@users.noreply.github.com>
2024-03-25 15:40:14 +0530

    Update service3.jsx

--- frontend/src/Components/Consultation/service3.jsx
+++ frontend/src/Components/Consultation/service3.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import ConsultationNavbar from './ConsultationNavbar'; // Adjust the import path based on your project structure
 import '../Consultation/service3.css'; // Import your CSS file for styling
+import AboutUs from './about'; 
 
 const Service3 = () => {
   return (
     <div className="service3-container">
+      
       <ConsultationNavbar />
 
       {/* Main Content */}
       <div className="main-content">
+      <Routes>
+            <Route path="/Consultation/about" element={<AboutUs />} /> {/* Route for AboutUs page */}
+            {/* Add routes for other pages */}
+          </Routes>
         {/* Your Online Consultation page content */}
         <h2>Welcome to the Dedibidi <br></br> 
          Consultation Service!!!</h2>
@@ -17,7 +24,7 @@ const Service3 = () => {
         <button>Explore More</button>
       </div>
 
-    
+     
     </div>
   );
 };
