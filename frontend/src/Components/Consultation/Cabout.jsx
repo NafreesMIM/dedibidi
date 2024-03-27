3b4d7fdaa913fcacb10613da7bc7e23db975075d
Adithya Ruwanpura <127475434+Adithyaruwanpura@users.noreply.github.com>
2024-03-27 15:33:37 +0530

    Create Cabout.jsx

---
+++ frontend/src/Components/Consultation/Cabout.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './Cabout.css';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Nullam ac urna vel enim ullamcorper laoreet. In hac habitasse platea dictumst.
+      </p>
+      {/* Add more content as needed */}
+    </div>
+  );
+};
+
+export default AboutUs;
