cf46b99413485e0247278476b7b71ad22ce3b5ee
Adithya Ruwanpura <127475434+Adithyaruwanpura@users.noreply.github.com>
2024-04-17 20:13:08 +0530

    Update service1.jsx

---
+++ frontend/src/Components/Job/service1.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import jobNav from './jobNav'; 
+import '../Job/service1.css'; 
+
+
+const Service1 = () => {
+  return (
+    <div className="service3-container">
+      
+      <jobNav />
+
+      {/* Main Content */}
+      <div className="main-content">
+     
+        
+       
+       
+      </div>
+
+     
+    </div>
+  );
+};
+
+export default Service1;
