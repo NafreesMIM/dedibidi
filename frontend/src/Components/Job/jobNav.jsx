99e3172595a5352c4ad7b15b71d9fe52e15b069a
Adithya Ruwanpura <127475434+Adithyaruwanpura@users.noreply.github.com>
2024-07-05 20:27:53 +0530

    Update jobNav.jsx

--- frontend/src/Components/Job/jobNav.jsx
+++ frontend/src/Components/Job/jobNav.jsx
@@ -1,42 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../job/jobNav.css';
+import '../Job/JobNav.css';
 import logo from '../Assets/Logo.png';
 
 const jobNavbar = () => {
   return (
     <div>
-      {/* Existing Navbar */}
+     
       <div className="Job-navbar">
         <div className="navbar-left">
           <Link to="/Job/home">
             <img src={logo} alt="Company Logo" className="logo" />
           </Link>
-          <h2>Dadibidi Jobs seeking & posting</h2>
+          <h1>Welcome to Dadibidi Jobs </h1>
         </div>
 
-        <div className="navbar-center">
-          {/* Add a search bar here */}
-          <input type="text" placeholder="Search..." />
-        </div>
-
+       
         <div className="navbar-right">
-          {/* Add login and signup links */}
+
           <Link to="/Job/login">Login</Link>
           <Link to="/Job/signup">Signup</Link>
         </div>
       </div>
 
-      {/* New Navbar for Service to Support */}
+     
       <div className="support-navbar">
-        <Link to="/Job/services">Service</Link>
-        <Link to="/Job/home">About Us</Link>
-        <Link to="/Job/services">FAQs</Link>
-        <Link to="/Job/services">Review</Link>
-        <Link to="/Job/services">Contact Us</Link>
-        <Link to="/Job/services">Calender</Link>
-        <Link to="/Job/services">Payment</Link>
-        <Link to="/Job/services">Support</Link>
+        <Link to="/Job/services">Home</Link>
+        <Link to="/Job/home">Post Your Vacancy</Link>
+        <Link to="/Job/services">Post your application</Link>
+        <Link to="/Job/services">Success stories</Link>
+        <Link to="/Job/services">Top employers</Link>
+        <Link to="/Job/services">Resources</Link>
+        <Link to="/Job/services">Contact us</Link>
+        <Link to="/Job/services">About us</Link>
       </div>
     </div>
   );
