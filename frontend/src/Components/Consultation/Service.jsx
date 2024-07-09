777fab95cc235a65260ea05382bef1bd1f3a37b7
Adithya Ruwanpura <127475434+Adithyaruwanpura@users.noreply.github.com>
2024-07-09 11:09:18 +0530

    Update Service.jsx

---
+++ frontend/src/Components/Consultation/Service.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './Service.css';
+
+const service = () => {
+  return (
+    <div className="about-us-con">
+      <h2>services</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Nullam ac urna vel enim ullamcorper laoreet. In hac habitasse platea dictumst.
+      </p>
+    
+    </div>
+  );
+};
+
+export default service;
