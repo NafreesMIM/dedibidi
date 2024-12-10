5c23271f0a4623bedd7cef0faa1a9203dce04e15
NafreesMIM <143109431+NafreesMIM@users.noreply.github.com>
2024-12-10 01:00:34 +0000

    connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 // Import required modules
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 // Import route handlers
@@ -11,17 +10,24 @@ const loginRoute = require('./Routes/login');
 // Create Express app
 const app = express();
 
+// Middleware
 app.use(express.json());
-app.use(cors()); // Allow all origins for demonstration purposes. You should configure it for specific origins.
+app.use(cors()); // Allow all origins for demonstration purposes
 
-mongoose.connect('mongodb://127.0.0.1:27017/dedibidi', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+// MongoDB connection
+const mongoURI = "mongodb+srv://mohamednafrees02:XYxvu5kuPrNA5Wfn@cluster0.sd2sh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-app.use('/api', registerRoute);
-app.use('/api', loginRoute);
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log("MongoDB Connection Error:", err));
+
+    // Routes
+    app.use('/api/register', registerRoute);
+    app.use('/api/login', loginRoute);
+
+    // Start the server
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+      });
+      
